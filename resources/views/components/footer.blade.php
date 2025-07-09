4a082bc9f71ae678f1650de45da3a1828f3a2f69
SimonPierreKouassi <simonkouassi33@gmail.com>
2025-07-09 17:55:41 +0000

    modification du footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -7,11 +7,10 @@
                     <svg class="h-8 w-8 text-orange-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3a4 4 0 118 0v4m-4 8a4 4 0 11-8 0V7a4 4 0 118 0v4z" />
                     </svg>
-                    <span class="ml-2 text-xl font-bold">RDV Manager</span>
+                    <span class="ml-2 text-xl font-bold">Gestionnaire de RDV</span>
                 </div>
                 <p class="text-gray-400 mb-4">
-                    Système de gestion de rendez-vous moderne et efficace pour optimiser 
-                    votre organisation et améliorer l'expérience utilisateur.
+                    Système de gestion de rendez-vous moderne et efficace.
                 </p>
             </div>
 
@@ -52,14 +51,14 @@
                         <svg class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                         </svg>
-                        <span>+225 XX XX XX XX</span>
+                        <span>07 15 96 96 96 - 07 15 88 88 88 - 07 16 88 88 88</span>
                     </div>
-                    <div class="flex items-center">
+                   <!-- <div class="flex items-center">
                         <svg class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                         </svg>
-                        <span>contact@rdvmanager.com</span>
-                    </div>
+                        <span></span>
+                    </div> --> 
                     <div class="flex items-center">
                         <svg class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
@@ -73,7 +72,7 @@
 
         <!-- Copyright -->
         <div class="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; {{ date('Y') }} RDV Manager. Tous droits réservés.</p>
+            <p>&copy; {{ date('Y') }} Agefop ©. Tous droits réservés.</p>
         </div>
     </div>
 </footer>

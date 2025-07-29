f8905228022f92baf1951ab73b96fac017f68345
SimonPierreKouassi <simonkouassi33@gmail.com>
2025-07-29 12:10:08 +0000

    correction cspmycustom

--- app/Policies/CspMyCustomPolicy.php
+++ app/Policies/CspMyCustomPolicy.php
@@ -4,30 +4,16 @@ namespace App\Policies;
 
 use Spatie\Csp\Directive;
 use Spatie\Csp\Keyword;
-use Spatie\Csp\Policy as CspPolicy;
+use Spatie\Csp\Presets\Preset;
 
-class CspMyCustomPolicy extends CspPolicy
+class CspMyCustomPolicy implements Preset
 {
-    public function configure(): void
+    public function configure(): array
     {
-        // Add default directives first if you want them
-        parent::create(); // Uncomment this line to include basic directives like 'self' for scripts, styles, etc.
-
-        // Block framing from any other origin
-        $this->add(Directive::FRAME_ANCESTORS, Keyword::NONE);
-
-        // Or, to allow only your own domain to frame it:
-        // $this->addDirective(Directive::FRAME_ANCESTORS, Keyword::SELF);
-
-        // Or, to allow specific domains:
-        // $this->addDirective(Directive::FRAME_ANCESTORS, [
-        //     'https://your-trusted-subdomain.com',
-        //     'https://another-internal-app.com',
-        // ]);
-        
-
-        // Example: If you also want to block scripts from unknown sources (good practice!)
-        $this->add(Directive::SCRIPT, Keyword::SELF);
-        $this->add(Directive::STYLE, Keyword::SELF);
+        return [
+            Directive::FRAME_ANCESTORS => [Keyword::NONE],
+            Directive::SCRIPT_SRC => [Keyword::SELF],
+            Directive::STYLE_SRC => [Keyword::SELF],
+        ];
     }
 }
